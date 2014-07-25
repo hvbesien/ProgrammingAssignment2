55b2f31f960d8e02bae6f9fce692c634086bf866
Herman van Besien <hvbesien@gmail.com>
2014-07-25 17:03:49 -0400

    made the makeCacheMatrix function and wrote notes

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## The matrix will be stored in the functions of my makeCacheMatrix class
+## There is a separate function cacheSolve that actually solves the inverse, but
+## the inverse is stored in makeCacheMatrix.
+## The code is modeled off RD Peng's makeVector and cachemean functions.
 
 ## Write a short comment describing this function
+## makeCacheMatrix includes set, get, setinv, and getinv functions
+## It does no computation, simply serves as a 'home' for the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y){           #changes the matrix
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x           #returns the matrix
+	setinv <- function(inverse){  #changes the inverse
+		inv <<- inverse
+	}
+	getinv <- function() inv      #returns the inverse
+	list(set = set, get = get,    #somewhat arbitrary return value
+		setinv = setinv,
+		getinv = getinv)
 }
 
 
